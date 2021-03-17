10443e014a102b80e9c1e731a969f6e343fd90df
Lisa <lisa.mieth@gmx.de>
2021-03-17 17:25:01 +0100

    Initial React setup

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return <h1>Ancestry React app</h1>
+  }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const webpack = require('webpack')
+ 
+module.exports = {
+  entry: path.resolve(__dirname, './index.js'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
+  },
+}
