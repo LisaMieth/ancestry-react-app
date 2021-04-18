6dc9ceb3cf0b058a0754442fe26755b943b3e521
Lisa <lisa.mieth@gmx.de>
2021-04-18 09:30:19 +0200

    Remove legacy code

--- src/app/store.js
+++ src/app/store.js
@@ -1,24 +1,5 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
-import thunkMiddleware from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit'
 import allReducer from './allSlice'
-
-// const rootReducer = combineReducers({
-//   all: allReducer,
-// })
-
-// export default function configureAppStore(preloadedState) {
-//   const store = configureStore({
-//     reducer: rootReducer,
-//     middleware: [thunkMiddleware, ...getDefaultMiddleware()],
-//     preloadedState,
-//   })
-
-//   if (process.env.NODE_ENV !== 'production' && module.hot) {
-//     module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
-//   }
-
-//   return store
-// }
 
 export default configureStore({
   reducer: {
