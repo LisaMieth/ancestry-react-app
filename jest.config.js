1459f4ad70c8b7263cf018893ca7def75cb68093
Lisa <lisa.mieth@gmx.de>
2025-06-07 14:44:20 +0200

    build setting tweaks & Node version update

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ const config = {
   verbose: true,
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   testEnvironment: 'jsdom',
+  transform: { '\\.[j]sx?$': 'babel-jest' },
+  testPathIgnorePatterns: ['./node_modules/'],
+  extensionsToTreatAsEsm: ['.esm.js'],
 }
 
 module.exports = config
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import EnvironmentPlugin from 'vite-plugin-environment'
 
 export default defineConfig({
   plugins: [EnvironmentPlugin('all')],
+  build: {
+    target: 'esnext',
+  },
 })
